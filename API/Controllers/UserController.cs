99663b06aa4bb9c3eac20631098d53164e229f8b
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-06-25 21:32:39 -0600

    Agregar archivos de proyecto.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using Models.Entities;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDBContext _db;
+
+        public UserController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Usuario>> GetUsuarios()
+        {
+            var usuarios = _db.Users.ToList();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Usuario> GetUsuario(int id)
+        {
+            var usuario = _db.Users.Find(id);
+            return Ok(usuario);
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Users { get; set; }
+    }
+}
